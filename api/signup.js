d005d442d8ecbe22ef389c8e42f772a0f3617cd1
Thadeu Cavalcanti <thadeu.ct@gmail.com>
2025-10-23 22:20:24 -0300

    inserção de preferencias atualizada

--- api/signup.js
+++ api/signup.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs"; // Biblioteca para hashing de senhas
-import { supabase } from "../lib/database.js"; // Importa o cliente Supabase configurado
+import { supabase, supabaseAdmin } from "../lib/database.js"; // Importa o cliente Supabase configurado
 
 export default async function handler(req, res) { 
   // Configura os cabeçalhos CORS para permitir requisições do frontend
@@ -22,20 +22,32 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "A senha deve ter no mínimo 6 caracteres" });
   }
 
+  let novoUsuarioId = null;
+
   try { // Verifica se o e-mail já está cadastrado
     // sistema para criptografia de senha
     const fatorSecreto = 15;
     const senhaHash = await bcrypt.hash(senha, fatorSecreto);
 
     // Insere o novo usuário na tabela "usuarios"
-    const { data: novoUsuario, error: insertError } = await supabase 
+    const { data: novoUsuario, error: insereUsuaioError } = await supabase 
       .from("usuarios")
       .insert([{ nome, sobrenome, email, senha_hash: senhaHash }])
       .select("id, nome, sobrenome, email, created_at")
       .single();
     
-    if (insertError) {
-      throw insertError; // manda erro para o catch
+    if (insereUsuaioError) {
+      throw insereUsuaioError; // manda erro para o catch
+    }
+
+    novoUsuarioId = novoUsuario.id;
+
+    const { error: inserePreferenciaError } = await supabaseAdmin
+      .from("preferencias")
+      .insert({id_usuario: novoUsuarioId})
+
+    if (inserePreferenciaError) {
+      throw inserePreferenciaError; // manda erro para o catch
     }
 
     // pop-up de sucesso
@@ -46,6 +58,13 @@ export default async function handler(req, res) {
     if (err.code === '23505') {
         return res.status(400).json({ error: "Este e-mail já está cadastrado." });
     }
+    
+    if (novoUsuarioId) {
+      console.error(`Erro ao concluir a conta para ${novoUsuarioId}. Iniciando rollback...`);
+      await supabaseAdmin.auth.admin.deleteUser(novoUsuarioId);
+      console.log(`Rollback concluído: Usuário ${novoUsuarioId} deletado.`);
+    }
+
     res.status(500).json({ error: `Erro no servidor: ${err.message}` });
   }
 }
