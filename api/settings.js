a61f10b84e5ee3af0009f89e0f55ba8746bf0438
Thadeu Cavalcanti <thadeu.ct@gmail.com>
2025-11-20 11:38:54 -0300

    Inserção de autorização

--- api/settings.js
+++ api/settings.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   // Configura os cabeçalhos CORS para permitir requisições do frontend
   res.setHeader('Access-Control-Allow-Origin', 'https://thadeu-ct.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS'); 
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.status(204).end();
   } // backend responde as regras
